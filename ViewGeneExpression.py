342123a0d30d12bd07e44559c9d6e1f5460458c9
StuartJO <stuart.oldham@yahoo.com>
2022-07-20 08:52:30 +1000

    Update ViewGeneExpression.py

--- ViewGeneExpression.py
+++ ViewGeneExpression.py
@@ -1,4 +1,3 @@
-from cmath import nan
 import numpy as np
 import argparse
 from nilearn import plotting
@@ -8,6 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("SURFACE", help="file of the surface to plot the gene expression on (lh.white ; lh.pial ; lh.inflated)")
 parser.add_argument("GENEMAP", help=".gii file of gene expression to project onto the surface")
 parser.add_argument("--parc",help="file of a parcellation to use for smoothing")
+parser.add_argument("--parc_rescale", help="Rescale after applying the parcellation",type=int)
 args = parser.parse_args()
 
 data=surface.load_surf_data(args.GENEMAP)
@@ -31,6 +31,12 @@ if args.parc:
     for i in range(1, nparc+1):
         nverts = len(np.extract(parc==i,parc))
         data_scaled[parc == i] = sum(data_scaled[parc == i])/nverts
+        if args.parc_rescale == 1:
+            data_nonzero = np.extract(data_scaled>0,data_scaled)
+            min_value = min(data_nonzero)
+            max_value= max(data_nonzero)
+            data_scaled = scale_min + (((data_scaled-min_value)/(max_value-min_value))*(scale_max-scale_min))
+            data_scaled[data == 0]=0
 
 view = plotting.view_surf(args.SURFACE, data_scaled,cmap='turbo', symmetric_cmap=False,threshold=scale_min/2)
 
